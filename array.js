171b1b0c6363dd4cc2d77ffdbcf178e7f862a1ac
Sidharth Falodia <189434527+Sidharth-max@users.noreply.github.com>
2025-09-20 17:54:38 +0530

    go

---
+++ array.js
@@ -0,0 +1,8 @@
+
+
+arr=[10,20,30,40,50];
+
+
+console.log(arr.sort( (a,b)=>b-a ));
+console.log(arr.reverse());
+console.log(arr.includes(50));
---
+++ string.js
@@ -0,0 +1,7 @@
+let string = "Hello, World!" +
+    "\nWelcome to JavaScript string manipulation.";
+console.log(string.toUpperCase());
+console.log(string.toLowerCase());
+console.log(string.length);
+console.log(string.includes("World"));
+console.log(string.replaceAll(/Hello/ig, "Hi"));
