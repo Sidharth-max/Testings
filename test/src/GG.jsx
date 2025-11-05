f31fb16a7f418a346d233687ab1465e7a51de4d8
Sidharth Falodia <189434527+Sidharth-max@users.noreply.github.com>
2025-11-05 15:24:43 +0530

    n

--- test/src/GG.jsx
+++ test/src/GG.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import Button from '@mui/material/Button';
+import styled from 'styled-components';
+
+const H2 = styled.h2`
+  color: blue;
+  font-size: 24px;
+  text-align: center;
+  margin: 20px 0;
+  background-color: lightgray;
+`;
 
 function GG() {
+  const userName = "GG";
+
   return (
     <div>
+      <H2>{userName}</H2>
       <Button variant="contained" color="primary">
         Click Me
       </Button>
+      
     </div>
   );
 }
-
 export default GG;
