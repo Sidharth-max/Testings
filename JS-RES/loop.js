9b5dbe1d0a82c0d96ba9eea781281febeb5775f4
Sidharth Falodia <189434527+Sidharth-max@users.noreply.github.com>
2026-03-26 10:57:51 +0530

    commit new!!

---
+++ JS-RES/loop.js
@@ -0,0 +1,8 @@
+   let i = 10
+    while (i <= 10) {
+        console.log(i);
+        i++;
+    }
+    for (let i = 0; i <= 10; i++) {
+        console.log(i);
+    }
---
+++ JS-RES/sum of digiths.js
@@ -0,0 +1,10 @@
+let no = 124
+let count = 0
+while (no <= no) {
+    let r = no%10
+    no = Math.floor(no/10)
+    count++
+}
+
+console.log(count)
+console.log(no)
